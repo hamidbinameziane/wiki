034dc0980e39afed679099a9aa5dd16a24725ab5
hamidbinameziane <abuabdellahhamid@gmail.com>
2023-11-25 18:11:08 +0100

    first commit

---
+++ encyclopedia/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render
+import markdown2
+
+from . import util
+
+
+def index(request):
+    return render(request, "encyclopedia/index.html", {
+        "entries": util.list_entries()
+    })
+
+def entry_page(request, title):
+    entry = util.get_entry(title.capitalize())
+    if entry != None:
+        entry_send = markdown2.markdown(entry)
+        print(entry_send)
+        return render(request, "encyclopedia/entry_page.html", {
+            
+            "entry": entry_send
+        })
+    else:
+        entry = util.get_entry(title.upper())
+        if entry != None:
+            entry_send = markdown2.markdown(entry)
+            return render(request, "encyclopedia/entry_page.html", {
+                
+                "entry": entry_send
+            })
+        else:
+            return render(request, "encyclopedia/entry_page.html", {
+                
+                "entry": ' the requested page was not found'
+            })
+
+
