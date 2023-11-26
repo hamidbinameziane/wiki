657988334b49813b6ed8adadfe50cf6448df07b6
hamidbinameziane <abuabdellahhamid@gmail.com>
2023-11-27 00:12:50 +0100

    implementing search form

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("wiki", views.index, name="index"),
     path("", views.index, name="index"),
-    path("<str:title>", views.entry_page, name="entry_page")
+    path("<str:title>", views.entry_page, name="entry_page"),
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,22 +1,74 @@
 from django.shortcuts import render
 import markdown2
+from django import forms
 
 from . import util
 
+class SearchForm(forms.Form):
+    search = forms.CharField(label="", widget=forms.TextInput({ "placeholder": "Search Encyclopedia",'class': 'search'}))
+
 
 def index(request):
+    if request.method == "POST":
+        lst = []
+        form = SearchForm(request.POST)
+        if form.is_valid():
+            query = form.cleaned_data["search"].lower()
+            entries = util.list_entries()
+            for entry in entries:
+                if query == entry.lower():
+                    entry_t = util.get_entry(entry)
+                    entry_send = markdown2.markdown(entry_t)
+                    return render(request, "encyclopedia/entry_page.html", {
+                        "title": entry,
+                        "entry": entry_send,
+                        "form": SearchForm()
+                    })
+                elif query in entry.lower():
+                    lst.append(entry)
+            return render(request, "encyclopedia/search_page.html", {
+                "lst": lst,
+                "form": SearchForm()
+            })
+
+
+
+
     return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
+        "entries": util.list_entries(),
+        "form": SearchForm()
     })
 
 def entry_page(request, title):
+    if request.method == "POST":
+        lst = []
+        form = SearchForm(request.POST)
+        if form.is_valid():
+            query = form.cleaned_data["search"].lower()
+            entries = util.list_entries()
+            for entry in entries:
+                if query == entry.lower():
+                    entry_t = util.get_entry(entry)
+                    entry_send = markdown2.markdown(entry_t)
+                    return render(request, "encyclopedia/entry_page.html", {
+                        "title": entry,
+                        "entry": entry_send,
+                        "form": SearchForm()
+                    })
+                elif query in entry.lower():
+                    lst.append(entry)
+            return render(request, "encyclopedia/search_page.html", {
+                "lst": lst,
+                "form": SearchForm()
+            })
+
     entry = util.get_entry(title.capitalize())
     if entry != None:
         entry_send = markdown2.markdown(entry)
-        print(entry_send)
         return render(request, "encyclopedia/entry_page.html", {
             "title": title.capitalize(),
-            "entry": entry_send
+            "entry": entry_send,
+            "form": SearchForm()
         })
     else:
         entry = util.get_entry(title.upper())
@@ -24,12 +76,15 @@ def entry_page(request, title):
             entry_send = markdown2.markdown(entry)
             return render(request, "encyclopedia/entry_page.html", {
                 "title": title.upper(),
-                "entry": entry_send
+                "entry": entry_send,
+                "form": SearchForm()
             })
         else:
             return render(request, "encyclopedia/entry_page.html", {
                 
-                "entry": ' the requested page was not found'
+                "entry": ' the requested page was not found',
+                "form": SearchForm()
             })
+        
 
 
